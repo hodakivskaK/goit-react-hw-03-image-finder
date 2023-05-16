0a0c5651d64bdb5d7cabec751b1d28b1d2df0ae1
Chris <kisunya750@gmail.com>
2023-05-16 19:53:11 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,5 +4,10 @@ import c from './Button.module.css'
 
 export const Button = ({onConsole}) => {
     
-    return <button type="button" className={c.Button} onClick={onConsole}  >Load more</button>
+    return <button type="button" className={c.Button} onClick={onConsole} >Load more</button>
 }
+
+
+Button.propTypes = {
+    onConsole: PropTypes.func
+};
